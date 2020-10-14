38ba114984a981131c04e4cd559fdc6bb4155c85
Doug Campos <douglas.campos@shopify.com>
2020-10-14 11:46:18 -0400

    add a bunch of vim plugins

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -17,6 +17,15 @@ let
     nixpkgs-fmt
     ripgrep
   ];
+  tpope.vim-rails = pkgs.vimUtils.buildVimPlugin {
+    name = "vim-rails";
+    src = pkgs.fetchFromGitHub {
+      owner = "tpope";
+      repo = "vim-rails";
+      rev = "2c42236cf38c0842dd490095ffd6b1540cad2e29";
+      sha256 = "0nhf4qd7dchrzjv2ijcddav72qb121c9jkkk06agsv23l9rb31pv";
+    };
+  };
 in
 {
   # Let Home Manager install and manage itself.
@@ -27,4 +36,26 @@ in
   programs.fzf.enable = true;
   programs.fzf.enableZshIntegration = true;
   programs.htop.enable = true;
+  programs.neovim = {
+    enable = true;
+    plugins = with pkgs.vimPlugins; [
+      editorconfig-vim
+      emmet-vim
+      fzf-vim
+      nerdcommenter
+      nerdtree
+      nord-vim
+      tpope.vim-rails
+      typescript-vim
+      vim-airline
+      vim-airline-themes
+      vim-endwise
+      vim-fugitive
+      vim-gitgutter
+      vim-nix
+      vim-ruby
+      vim-surround
+      vim-terraform
+    ];
+  };
 }
