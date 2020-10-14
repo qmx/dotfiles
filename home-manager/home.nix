28c1a889139bde93cdd907639316e4c79b928ae8
Doug Campos <douglas.campos@shopify.com>
2020-10-14 10:39:48 -0400

    start moving things over to nix

---
+++ home-manager/home.nix
@@ -0,0 +1,26 @@
+{ config, pkgs, ... }:
+let
+  LS_COLORS = pkgs.fetchgit {
+    url = "https://github.com/trapd00r/LS_COLORS";
+    rev = "6fb72eecdcb533637f5a04ac635aa666b736cf50";
+    sha256 = "0czqgizxq7ckmqw9xbjik7i1dfwgc1ci8fvp1fsddb35qrqi857a";
+  };
+  ls-colors = pkgs.runCommand "ls-colors" { } ''
+    mkdir -p $out/bin $out/share
+    ${pkgs.coreutils}/bin/dircolors -b ${LS_COLORS}/LS_COLORS > $out/share/DIR_COLORS
+  '';
+  localPackages = [ ls-colors ];
+  packages = with pkgs; [
+    nixpkgs-fmt
+  ];
+in
+{
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+  programs.home-manager.path = "~/dev/dotfiles/home-manager/home.nix";
+  home.packages = localPackages ++ packages;
+  programs.bat.enable = true;
+  programs.fzf.enable = true;
+  programs.fzf.enableZshIntegration = true;
+  programs.htop.enable = true;
+}
