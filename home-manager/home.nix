385e5783b1ae0d6dd1e427b235ee18da93a381b3
Doug Campos <douglas.campos@shopify.com>
2020-10-17 13:09:04 -0400

    just using exa instead of ls + ls_colors

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,15 +1,5 @@
 { config, pkgs, ... }:
 let
-  LS_COLORS = pkgs.fetchgit {
-    url = "https://github.com/trapd00r/LS_COLORS";
-    rev = "6fb72eecdcb533637f5a04ac635aa666b736cf50";
-    sha256 = "0czqgizxq7ckmqw9xbjik7i1dfwgc1ci8fvp1fsddb35qrqi857a";
-  };
-  ls-colors = pkgs.runCommand "ls-colors" { } ''
-    mkdir -p $out/bin $out/share
-    ${pkgs.coreutils}/bin/dircolors -b ${LS_COLORS}/LS_COLORS > $out/share/DIR_COLORS
-  '';
-  localPackages = [ ls-colors ];
   packages = with pkgs; [
     bat
     exa
@@ -37,7 +27,7 @@ in
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
   programs.home-manager.path = "~/dev/dotfiles/home-manager/home.nix";
-  home.packages = localPackages ++ packages;
+  home.packages = packages;
   programs.bat.enable = true;
   programs.fzf.enable = true;
   programs.fzf.enableZshIntegration = true;
@@ -135,7 +125,6 @@ in
         source /opt/dev/dev.sh
       fi
 
-      eval $(cat ~/.nix-profile/share/DIR_COLORS)
       eval "$(zoxide init zsh)"
       eval "$(starship init zsh)"
     '';
