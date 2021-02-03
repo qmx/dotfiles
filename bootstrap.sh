a78eff53d9f8073a409d2b3f5a5ef7fc106a617b
Doug Campos <douglas.campos@shopify.com>
2021-02-03 11:41:46 -0500

    fix tyop

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-mkdir -p ~/.config/nix && echo "sandbox = false" > ~/.config/nix/nix.conf"
+mkdir -p ~/.config/nix && echo "sandbox = false" > ~/.config/nix/nix.conf
 curl -L https://nixos.org/nix/install | sh
+. ~/.nix-profile/etc/profile.d/nix.sh
 
 ./switch.sh
