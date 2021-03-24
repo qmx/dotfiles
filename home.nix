906891389d7d5f39b4a6a8227b4587c4bdf43e3b
Doug Campos <qmx@qmx.me>
2021-03-24 08:53:31 -0400

    start packaging a okayish `rails new` with nix

--- home.nix
+++ home.nix
@@ -21,6 +21,60 @@ let
 
     doCheck = false;
   };
+  nrails = writeScriptBin "nrails" ''
+      if [ -z "$1" ]; then
+        echo "Usage: nrails APP_NAME"
+        exit 1
+      fi
+
+      APP_NAME="$1"
+
+      mkdir -p $APP_NAME
+      (
+      cd $APP_NAME
+      ${niv}/bin/niv init -b nixpkgs-unstable
+
+      cat <<EOS >Gemfile
+      source 'https://rubygems.org'
+      gem 'rails', '6.1'
+      EOS
+
+      cat <<EOS >bundler.nix
+      let
+        sources = import ./nix/sources.nix;
+        nixpkgs = sources."nixpkgs";
+      in
+        with (import nixpkgs {});
+      let
+        myBundler = bundler.override { ruby = ruby_2_7; };
+      in
+        mkShell {
+          name = "bundler-shell";
+          buildInputs = [ myBundler bundix ];
+        }
+      EOS
+
+      nix-shell --run "bundle lock" bundler.nix
+
+      cat <<EOS >shell.nix
+      let
+        sources = import ./nix/sources.nix;
+        nixpkgs = sources."nixpkgs";
+      in
+        with (import nixpkgs {});
+      let
+        env = bundlerEnv {
+          name = "$APP_NAME";
+          ruby = ruby_2_7;
+          gemdir = ./.;
+        };
+      in mkShell { buildInputs = [ env env.wrappedRuby ]; }
+      EOS
+
+      rm -f gemset.nix
+      nix-shell -p bundix --run "bundix"
+      )
+    '';
 
   packages = [
     ctags
@@ -33,6 +87,7 @@ let
     mosh
     niv
     nixpkgs-fmt
+    nrails
     ripgrep
     tokei
     ulidgen
