f2c99dc26a07d2587233f282039dd6034b1e8119
Doug Campos <qmx@qmx.me>
2021-04-15 20:59:08 -0400

    add vim keys for switching panes

--- home.nix
+++ home.nix
@@ -101,6 +101,11 @@ in
       bind | split-window -h
       bind - split-window -v
 
+      bind h select-pane -L
+      bind j select-pane -D
+      bind k select-pane -U
+      bind l select-pane -R
+
       bind -r H resize-pane -L 5
       bind -r J resize-pane -D 5
       bind -r K resize-pane -U 5
