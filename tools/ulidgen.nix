106511f4dc9ae8b6056f9caf602fcb51de729b53
Doug Campos <qmx@qmx.me>
2021-06-15 17:32:50 -0400

    update sha

--- tools/ulidgen.nix
+++ tools/ulidgen.nix
@@ -9,7 +9,7 @@ rustPlatform.buildRustPackage rec {
     sha256 = "0441sx02hq5nf2jldnhb304rya0gfsvv43665bnfdbxjfh7iwpq7";
   };
 
-  cargoSha256 = "1wjzxjicclny7r5vwhzq7scphrfi9lapdvxysbm5yr7jl274y4ry";
+  cargoSha256 = "195krg74z17n0j4rwmrv2w5swsnim7j53xpnbmy6mmq2bwcqqidi";
 
   buildInputs = lib.optionals stdenv.isDarwin [
     darwin.apple_sdk.frameworks.Foundation
