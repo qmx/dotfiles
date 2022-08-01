b0a296e52037307d24968f7cfc5a911c3eee0868
Doug Campos <qmx@qmx.me>
2022-08-01 15:52:24 -0400

    refactor finicky config

--- .finicky.js
+++ .finicky.js
@@ -1,44 +1,47 @@
+const CHROME_DOMAINS = [
+  "youtu.be",
+  "youtube.com",
+  "google.com",
+  "tailscale.com",
+];
+
+const SAFARI_DOMAINS = ["fly.io", "github.com", "amazon.com"];
+
 module.exports = {
-    defaultBrowser: "Brave Browser",
-    rewrite: [
-        {
-            match: ({url}) => url.host.endsWith("twitter.com"),
-            url: ({url}) => {
-                return {
-                    ...url,
-                    host: "nitter.net"
-                }
-            }
+  defaultBrowser: "Brave Browser",
+  rewrite: [
+    {
+      match: ({ url }) => url.host.endsWith("twitter.com"),
+      url: ({ url }) => {
+        return {
+          ...url,
+          host: "nitter.net",
+        };
+      },
+    },
+  ],
+  handlers: [
+    {
+      match: ({ url }) => {
+        for (let domain of CHROME_DOMAINS) {
+          if (url.host.endsWith(domain)) {
+            return true;
+          }
         }
-    ],
-    handlers: [
-        {
-            match: /^https?:\/\/classroom\.google\.com.*$/,
-            browser: "Firefox"
-        },
-        {
-            match: /^https?:\/\/.*\.?google\.com.*$/,
-            browser: "Google Chrome"
-        },
-        {
-            match: /^https?:\/\/.*\.?github\.com.*$/,
-            browser: "Safari"
-        },
-        {
-            match: /^https?:\/\/.*\.?amazon\.com.*$/,
-            browser: "Safari"
-        },
-        {
-            match: /^https?:\/\/.*\.?fly\.io.*$/,
-            browser: "Safari"
-        },
-        {
-            match: /^https?:\/\/.*\.?tailscale\.com.*$/,
-            browser: "Google Chrome"
-        },
-        {
-            match: /^https?:\/\/.*\.?youtu\.be.*$/,
-            browser: "Google Chrome"
+        return false;
+      },
+      browser: "Google Chrome",
+    },
+    {
+      match: ({ url }) => {
+        for (let domain of SAFARI_DOMAINS) {
+          if (url.host.endsWith(domain)) {
+            return true;
+          }
         }
-    ]
-}
+        return false;
+      },
+      browser: "Safari",
+    },
+  ],
+};
