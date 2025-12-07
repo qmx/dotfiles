4e4b043e5b6dd6e4dad8119a6f19cb2130c18680
Doug Campos <qmx@qmx.me>
2025-12-07 16:57:27 -0500

    Format secrets.nix
    - Apply consistent Nix formatting

--- secrets.nix
+++ secrets.nix
@@ -3,13 +3,19 @@ let
   secretsPath = "${home}/.config/nix/secrets.json";
   tryReadSecrets = builtins.tryEval (builtins.fromJSON (builtins.readFile secretsPath));
 in
-  if tryReadSecrets.success
-  then tryReadSecrets.value
-  else builtins.trace "WARNING: secrets.json is encrypted or unavailable at ${secretsPath}. Run 'git-crypt unlock' to decrypt." {
-    braveApiKey = "";
-    orthancUrl = "http://localhost:8080";
-    homebridge = {
-      bridge = { pin = ""; username = ""; };
-      cameras = [];
-    };
-  }
+if tryReadSecrets.success then
+  tryReadSecrets.value
+else
+  builtins.trace
+    "WARNING: secrets.json is encrypted or unavailable at ${secretsPath}. Run 'git-crypt unlock' to decrypt."
+    {
+      braveApiKey = "";
+      orthancUrl = "http://localhost:8080";
+      homebridge = {
+        bridge = {
+          pin = "";
+          username = "";
+        };
+        cameras = [ ];
+      };
+    }
