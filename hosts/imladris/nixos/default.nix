781e9963699a24bce5ecc299267d36e1b0738d85
Doug Campos <qmx@qmx.me>
2025-12-29 20:31:18 -0500

    fix(imladris): merge user config to avoid dynamic attr conflict
    Combine extraGroups and initialHashedPassword into single block
    to prevent Nix evaluation error with duplicate dynamic attributes.

--- hosts/imladris/nixos/default.nix
+++ hosts/imladris/nixos/default.nix
@@ -17,11 +17,12 @@ in
 
   # Docker
   virtualisation.docker.enable = true;
-  users.users.${username}.extraGroups = [ "docker" ];
 
-  # Passwords (from secrets/secrets.json.age)
-  users.users.${username}.initialHashedPassword =
-    "$6$q2DwGVH4HSuss40a$9pTOHZ1vJ7gimEdnNflMuM/YyfY76LSDE1cBZVS4bb43fsHHoumrb2TWUXhfXnEEFfmCyv3dtq5EYEn371Hi/0";
+  # User config
+  users.users.${username} = {
+    extraGroups = [ "docker" ];
+    initialHashedPassword = "$6$q2DwGVH4HSuss40a$9pTOHZ1vJ7gimEdnNflMuM/YyfY76LSDE1cBZVS4bb43fsHHoumrb2TWUXhfXnEEFfmCyv3dtq5EYEn371Hi/0";
+  };
   users.users.root.initialHashedPassword = "$6$YX.HgOCY1BnEhuRH$PpG4rOUxQKu7vztIYJTt//h3WoINZjXaGmeKORfBdecahjo.vEIxQCoE7Mx0.F3vnIBQ0PBXLbr7OXhsd8/7p.";
 
   # NFS mounts (backups and media, not models)
